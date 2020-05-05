876a1045b560d246d0cdaeaea754e7630d515c3a
michaelmcdermott <mmcdermott011@gmail.com>
2020-05-04 22:36:44 -0700

    final submission

---
+++ Assets/TV_button.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV_button : MonoBehaviour
+{
+    private int count;
+    public GameObject TV_text;
+    public GameObject TV_audio;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+
+public void Activate(){
+  	Instantiate(TV_text);
+        //yield return new WaitForSeconds(2);
+        Instantiate(TV_audio);
+
+}
+
+   
+}
+
---
+++ Assets/Winning_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Winning_Script : MonoBehaviour
+{
+    [SerializeField] private Text Winner_Text;
+    public GameObject Winner_audio;
+    private int count;
+    public GameObject Trigger;
+    void Start()
+    {
+        count = 0;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && count==0);
+        {
+            Winner_Text.text = "YOU ESCAPED";
+            Instantiate(Winner_audio);
+            count += 1;
+            Destroy(Trigger);
+        }
+    }
+}
